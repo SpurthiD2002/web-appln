1329b33b2a6141625640b9af3b7491beb34dd145
SpurthiD2002 <spurthidreddy@gmail.com>
2025-07-05 12:50:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,19 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-
+@app.route('/register')
 def register():
-    return render_template('register.html')
+    return render_template('register')
 
-
+@app.route('/submit', methods=['POST'])
 def submit():
-    fname = request.register.get('register')
-    return redirect(url_for('welcome'))
+    fname = request.form.get('fname')
+    return redirect(url_for('welcome', fname=fname))
 
-
+@app.route('/welcome')
 def welcome():
-    return render_template('welcome.html', fname=register)
+    fname = request.args.get('fname')
+    return render_template('welcome.html', fname=fname)
 
 if __name__ == '__main__':
     app.run(debug=True)
